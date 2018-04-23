3ddb8e243213aedb4091dd5bffec91956fc2d681
Xu Chunyang <mail@xuchunyang.me>
2018-04-23 16:34:20 +0800

    Python Crash Course | Alien Invasion (12.7)

--- Python-Crash-Course/Alien Invasion/alien_invasion.py
+++ Python-Crash-Course/Alien Invasion/alien_invasion.py
@@ -12,10 +12,11 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- Python-Crash-Course/Alien Invasion/game_functions.py
+++ Python-Crash-Course/Alien Invasion/game_functions.py
@@ -2,14 +2,28 @@ import sys
 
 import pygame
 
-def check_events():
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type in (pygame.QUIT, pygame.MOUSEBUTTONDOWN):
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            sys.exit()
-
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+                
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图形，并切换到新屏幕"""
     # 每次循环都重绘屏幕
