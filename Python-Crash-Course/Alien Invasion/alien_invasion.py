f615b98fd07c269b51e4f63def4a3cf1138eed98
Xu Chunyang <mail@xuchunyang.me>
2018-04-23 23:00:51 +0800

    Python Crash Course | Alien Invasion (13.3)

--- Python-Crash-Course/Alien Invasion/alien_invasion.py
+++ Python-Crash-Course/Alien Invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -13,16 +14,17 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵")
-    # 创建飞船
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-    # 创建子弹编组
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
