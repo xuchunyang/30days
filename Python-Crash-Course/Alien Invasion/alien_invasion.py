977f74838c415cd4008ed5fe97b99e54a2d06fe0
Xu Chunyang <mail@xuchunyang.me>
2018-04-24 14:21:59 +0800

    Python Crash Course | Alien Invasion (14.3)

--- Python-Crash-Course/Alien Invasion/alien_invasion.py
+++ Python-Crash-Course/Alien Invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from alien import Alien
@@ -17,6 +18,7 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵")
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -25,16 +27,17 @@ def run_game():
     play_button = Button(ai_settings, screen, "Play")
 
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship,
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
                         aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                              bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
         
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
-                         play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button)
 
 
 run_game()
