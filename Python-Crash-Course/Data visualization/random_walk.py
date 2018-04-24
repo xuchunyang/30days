2d6fb92c46c87a02ded3b7c8a9afc899493bf02f
Xu Chunyang <mail@xuchunyang.me>
2018-04-24 22:45:07 +0800

    Python Crash Course | Data visualization (15.5)

--- Python-Crash-Course/Data visualization/random_walk.py
+++ Python-Crash-Course/Data visualization/random_walk.py
@@ -12,19 +12,21 @@ class RandomWalk():
         self.x = [0]
         self.y = [0] 
 
+    def get_step(self):
+        """生成一个随机步数"""
+        direction = choice((1, -1))
+        distance = choice((0, 1, 2, 3, 4))
+        step = direction * distance
+        return step
+        
     def fill_walk(self):
         """生成随机漫步的所有点"""
 
         while len(self.x) < self.num_points:
 
             # 决定前进方向以及沿这个方向前进的距离
-            x_direction = choice((1, -1))
-            x_distance = choice((0, 1, 2, 3, 4))
-            x_step = x_direction * x_distance
-
-            y_direction = choice((1, -1))
-            y_distance = choice((0, 1, 2, 3, 4))
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # 拒绝原地踏步
             if x_step == 0 and y_step == 0:
--- Python-Crash-Course/Data visualization/rw_visual.py
+++ Python-Crash-Course/Data visualization/rw_visual.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-rw = RandomWalk(50000)
+rw = RandomWalk(5000)
 rw.fill_walk()
 
 # 设置绘制窗口的尺寸 英寸
 plt.figure(figsize=(10, 6))
 
 point_numbers = list(range(rw.num_points))
-plt.scatter(rw.x, rw.y, c=point_numbers, cmap=plt.cm.Blues, s=1)
+plt.plot(rw.x, rw.y, linewidth=1)
 
 # 突出起点和终点
 plt.scatter(0, 0, c='green', s=100)
